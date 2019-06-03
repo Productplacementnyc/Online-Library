a06d7f77c8bce2f6c7cb8c0fd7390608a6430472
Productplacementnyc <50431594+Productplacementnyc@users.noreply.github.com>
2019-06-03 18:12:46 -0400

    Add files via upload

---
+++ David-online-library/inc/data.php
@@ -0,0 +1,181 @@
+<?php
+
+///Books
+$catalog[101] = [
+  "title" => "The Four Agreements",
+  "img" => "img/fouragreements.jpg",
+ "genre" => "Self-help",
+ "format" => "Paperback",
+ "year" => 1998,
+ "category" => "Books",
+ "authors" => [
+     "Don Miguel Ruiz"
+ ],
+ "publisher" => "1998 Viking Press",
+ "isbn" => '9782848992549'
+];
+
+$catalog[102] = [
+  "title" => "The Dispossessed",
+  "img" => "img/thedispossessed.jpg",
+ "genre" => "Science Fiction",
+ "format" => "Paperback",
+ "year" => 1974,
+ "category" => "Books",
+ "authors" => [
+     "Ursula K. Le Guin"
+ ],
+ "publisher" => "Harper & Row",
+ "isbn" => '0-06-012563-2'
+];
+
+$catalog[103] = [
+  "title" => "48 Laws of Power",
+  "img" => "img/48laws.jpg",
+ "genre" => "Self-help",
+ "format" => "Paperback",
+ "year" => 1998,
+ "category" => "Books",
+ "authors" => [
+     "Robert Greene"
+ ],
+ "publisher" => "1998 Viking Press",
+ "isbn" => '9782848992549'
+];
+$catalog[104] = [
+  "title" => "Monster",
+  "img" => "img/monster.png",
+ "genre" => "Fiction",
+ "format" => "Paperback",
+ "year" => 1997,
+ "category" => "Books",
+ "authors" => [
+     "Walter Dean Myers"
+ ],
+ "publisher" => "HarperCollins Publishing",
+ "isbn" => '9782883532489'
+];
+///Movies
+$catalog[201] = [
+ "title" => "He Got Game",
+ "img" => "img/hegotgame.jpg",
+ "genre" => "Sports,Drama",
+ "format" => "DVD",
+ "year" => 1998,
+ "category" => "Movies",
+ "director" => "Spike Lee",
+ "writers" => [
+     "Spike Lee"
+
+
+ ],
+ "stars" => [
+     "Denzel Washington",
+     "Ray Allen",
+     "Rosario Dawson",
+     "Ned Betty",
+     "Milla Jovovich"
+ ]
+];
+
+$catalog[202] = [
+ "title" => "Above the Rim",
+ "img" => "img/abovetherim.jpg",
+ "genre" => "Drama,Crime,Sports",
+ "format" => "DVD",
+ "year" => 1994,
+ "category" => "Movies",
+ "director" => "Jeff Pollack",
+ "writers" => [
+     "Jeff Pollack"
+
+ ],
+ "stars" => [
+     "Tupac Shakur",
+     "Duane Martin",
+     "Marlon Williams"
+ ]
+];
+
+$catalog[203] = [
+ "title" => "Ready Player One",
+ "img" => "img/playerone.jpg",
+ "genre" => "Sci-fi",
+ "format" => "DVD",
+ "year" => 2018,
+ "category" => "Movies",
+ "director" => "Steven Spielberg",
+ "writers" => [
+     "Zak Penn"
+
+
+ ],
+ "stars" => [
+     "Tye Sheriden",
+     "Olivia Cooke",
+     "Ben Mendelsohn"
+ ]
+];
+
+$catalog[204] = [
+ "title" => "Water world",
+ "img" => "img/waterworld.png",
+ "genre" => "Fantasy, Mystery",
+ "format" => "DVD",
+ "year" => 1995,
+ "category" => "Movies",
+ "director" => "Kevin Reynolds",
+ "writers" => [
+     "Peter Rade"
+
+
+ ],
+ "stars" => [
+     "Kevin Costner",
+     "Jeanne Tripplehorn",
+     "Sigourney Weaver",
+     "Tina Majorino",
+     "Jack Black"
+ ]
+];
+
+///Music
+$catalog[301] = [
+ "title" => "Graduation",
+ "img" => "img/graduation.png",
+ "genre" => "Hip-Hop",
+ "format" => "CD",
+ "year" => 2007,
+ "category" => "Music",
+ "artist" => "Kanye West"
+];
+
+$catalog[302] = [
+ "title" => "In My Lifetime",
+ "img" => "img/inmylifetime.png",
+ "genre" => "Hip-Hop",
+ "format" => "CD",
+ "year" => 1997,
+ "category" => "Music",
+ "artist" => "Jay-Z"
+];
+
+$catalog[303] = [
+ "title" => "Habits & Contradictions",
+ "img" => "img/habits.png",
+ "genre" => "Hip-Hop",
+ "format" => "CD",
+ "year" => 2012,
+ "category" => "Music",
+ "artist" => "Schoolboy Q"
+];
+
+$catalog[304] = [
+ "title" => "DAMN",
+ "img" => "img/damn.png",
+ "genre" => "Hip-Hop",
+ "format" => "CD",
+ "year" => 2018,
+ "category" => "Music",
+ "artist" => "Kendrick Lamar"
+];
